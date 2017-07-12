22e406ffac53be52baaf62abfa67db50c41ae61d
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-11 23:13:21 -0400

    como no se pudo implementar la descripción, se le puso un texto específico en cada hover

--- assets/js/main-proyectos.js
+++ assets/js/main-proyectos.js
@@ -27,11 +27,15 @@ $(document).ready(function(){
 			var linkPage = element.html_url;
 
 			//acortamos la descripcion
+			//CON ESTO EFECTIVAMENTE SE ACORTA LA DESCRIPCIÓN, SIN EMBARGO, AL IMPLEMENTARLA SE DESCONFIGURA TODO EL DOM, LO DEJO COMENTADO
 			var shortDescription = descripcion.slice(0,120) + "...</p>";
-			console.log("shortDescription: " + shortDescription)
+			console.log("shortDescription: " + shortDescription);
+
+			//utilizamos este texto de reemplazo a la descripción
+			var textReemplazo = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt omnis nisi excepturi earum at doloribus repellendus tempore atque eaque recusandae, aspernatur!</p>'
 
 			//container-works es la clase donde se anidará todo
-			$(".container-works").append('<a href="' + linkPage + '" target="_blank" class="work" id="work' + data.indexOf(element).toString() + '"><div class="cont-img" id="cont-img' + data.indexOf(element).toString() + '"><img src="' + imgTeaser + '" class="image img-responsive"></img><div class="overlay"><div class="text"><h5>' + titulo + '</h5>' + shortDescription + '</div></div></div><div class="cont-icons" id="cont-icons' + data.indexOf(element).toString() + '"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> <span>' + vistas + '</span>  <i class="fa fa-comment" aria-hidden="true"></i> <span>' + comentarios + '</span>  <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> <span>' + likes + '</span></div></a>');
+			$(".container-works").append('<a href="' + linkPage + '" target="_blank" class="work" id="work' + data.indexOf(element).toString() + '"><div class="cont-img" id="cont-img' + data.indexOf(element).toString() + '"><img src="' + imgTeaser + '" class="image img-responsive"></img><div class="overlay"><div class="text"><h5>' + titulo + '</h5>' + textReemplazo /*shortDescription*/ + '</div></div></div><div class="cont-icons" id="cont-icons' + data.indexOf(element).toString() + '"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> <span>' + vistas + '</span>  <i class="fa fa-comment" aria-hidden="true"></i> <span>' + comentarios + '</span>  <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> <span>' + likes + '</span></div></a>');
 
 		});
 	}
