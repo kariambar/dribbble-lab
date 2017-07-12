917bfb663ad1ed8e0d293713868693ddaba523b7
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-11 22:48:01 -0400

    se acortó la descripción con slice, pero cuando se implementa, se desconfigura todo el dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 	});
 
 	function works(data){
-		console.log("holi");
 		data.forEach(function(element){
 
 			//variables necesarias
@@ -27,23 +26,15 @@ $(document).ready(function(){
 			var comentarios = element.comments_count;
 			var linkPage = element.html_url;
 
+			//acortamos la descripcion
+			var shortDescription = descripcion.slice(0,120) + "...</p>";
+			console.log("shortDescription: " + shortDescription)
+
 			//container-works es la clase donde se anidará todo
-
-			$(".container-works").append('<a href="' + linkPage + '" target="_blank" class="work" id="work' + data.indexOf(element).toString() + '"><div class="cont-img" id="cont-img' + data.indexOf(element).toString() + '"><img src="' + imgTeaser + '" class="image img-responsive"></img><div class="overlay"><div class="text"><p>' + titulo + '</p><p class="desc">holi</p></div></div></div><div class="cont-icons" id="cont-icons' + data.indexOf(element).toString() + '"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span><span>' + vistas + '</span><span class="glyphicon glyphicon-heart" aria-hidden="true"></span><span>' + likes + '</span><i class="fa fa-comment" aria-hidden="true"></i><span>' + comentarios + '</span></div></a>');
-
-			/*var containerHeight = $(".text").height();
-			console.log($(".text").height())
-			console.log(descripcion.length);
-
-			while ( descripcion.length > containerHeight ){
-				$(".desc").text(function (index, text) {
-					return text.replace(/\W*\s(\S)*$/, '...');
-				});
-			}*/
+			$(".container-works").append('<a href="' + linkPage + '" target="_blank" class="work" id="work' + data.indexOf(element).toString() + '"><div class="cont-img" id="cont-img' + data.indexOf(element).toString() + '"><img src="' + imgTeaser + '" class="image img-responsive"></img><div class="overlay"><div class="text"><h5>' + titulo + '</h5>' + shortDescription + '</div></div></div><div class="cont-icons" id="cont-icons' + data.indexOf(element).toString() + '"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> <span>' + vistas + '</span>  <i class="fa fa-comment" aria-hidden="true"></i> <span>' + comentarios + '</span>  <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> <span>' + likes + '</span></div></a>');
 
 		});
 	}
-
 
 });
 
